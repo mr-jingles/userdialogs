635e5d2d1ecd827ac602574272867a8975a27a01
Allan Ritchie <allan.ritchie@gmail.com>
2017-07-04 13:22:00 -0400

    minor

---
+++ src/Plugin.UserDialogs.iOS/ActionSheetDialog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Plugin.UserDialogs
+{
+    public class ActionSheetDialog : AbstractDialog, IActionSheetDialog
+    {
+        public IActionButton AddButton()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
